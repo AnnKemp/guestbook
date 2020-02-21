c58ceed3b0097867daa9b214b08ffbd611e7d2b8
AnnKemp <ann.kemp@scarlet.be>
2020-02-21 17:01:10 +0100

    most recent version

--- index.php
+++ index.php
@@ -3,92 +3,39 @@ declare(strict_types=1);
 ini_set('display_errors', "1"); // om foutmeldingen te tonen
 session_start();
 
-// class post
-class post {
 
-    // initializing properties
-    private $author;
-    private $message_content;
-    private $message_title;
+// --------CONTROLLER----------------------
+require ('classes.php');
+require ('class_guestbook.php');
 
-    // this is the setter
-    public function __construct($titel, $message, $name) {
-        $this->message_title=$titel;
-        $this->message_content=$message;
-        $this->author = $name;
-    }
-    // this is the getter
-    public function display() {
-        echo $this->message_title;
-        echo $this->message_content;
-        return $this->author;
-    }
-}
+$file = 'data.json'; // nu in het object
 
-$publication = new post($_POST['title'],$_POST['message'],$_POST['name']);
-$publication->display();
-echo '<br>';
-var_dump($publication);
+$publication = new post($_POST['title'],$_POST['date'],$_POST['message'],$_POST['name']);
+$post_values=$publication->display();
+// make the post-array to unshift the new post values into the data-array from the jason file
+$array_post=array("date"=>$post_values[1],"titel"=>$post_values[0],"comment"=>$post_values[2],"name"=>$post_values[3]);
 
-//var_dump($_POST); // testing $_POST
+$objectGuestbook=new guestbook($array_post, $file);
+$post_values=$objectGuestbook->display_data();
 
-/* zoiets gebruiken om de posts af te spelen: zie hieronder: ne foreach en dan ineens in de html plaatsen
-<?php foreach ($products AS $i => $product): ?>
-             <label><!-- hier nog den checked verbeteren want ie houdt alleen het laatste item geselecteerd !!!!!!!!!!!! nog aanpassen !!!!!! -->
-              <input type="checkbox" value="<?php echo number_format($product['price'], 2) ?>" name="products[<?php echo $product['name'] ?>]"
-               <?php if (isset($_POST['products']) && $product['name']==$foodDrink) echo "checked=checked";?>/> <?php echo $product['name'] ?> -
-                     &euro; <?php echo number_format($product['price'], 2) ?></label><br />
-                <?php endforeach; ?>*/
+file_put_contents($file, $objectGuestbook->backToJsonFile());  // put the data back on the json file
 
-// nen datum genereren om ook in dat gastboek te publiceren!
-echo $date = date('d-m-Y'); // generate the dat for in the guestbook
-// class guestbook
-// only show the latest 20 post - newest message on top
-// get and put data on a file:
-
-$file = 'data.json';
 //read json file from url in php
-$readJSONFile = file_get_contents($file); // dit werkt! :)
+//$readJSONFile = file_get_contents($file); // dit werkt! :)
 //print_r($readJSONFile); // display contents
-
-$array = substr($readJSONFile,7,2);
+//$array = substr($readJSONFile,7,2); // remove unwanted characters
 //convert json to array in php
-$array = json_decode($readJSONFile, TRUE); // makes an array // dit werkt! :)
-var_dump($array); // print array
-
-// in een foreach steken om alles te tonen
-foreach ($array AS $key => $value):
-    echo "<h3>$value['titel']</h3>";
-    echo "<h3>$value</h3>";
-
-                 endforeach;
-echo $array[0]['date'];
-echo $array[0]['titel'];
-echo $array[0]['comment'];
-echo $array[0]['name'];
-
-// dus als de post verstuurt wordt onclick via unshift de gegevens van boven bij duwen.  de tweede waarde nog vervangen door een var uit de post
-$array_post=array("date"=>"2-3-2020","titel"=>"verzonnen titel","comment"=>"blablabla","name"=>"Steve");
-array_unshift($array,$array_post);
-// om data om te zetten naar tekst voor deze terug op te slaan
-$content=json_encode($array);
-var_dump($content);
-
-file_put_contents($file, $content);
-
+//$array = json_decode($readJSONFile, TRUE); // makes an array // dit werkt! :)
 //var_dump($array); // print array
-/*$file = 'data.json';
-$content = file_get_contents($file);
-json_encode() //to convert your array to a string to store. // om die array om te zetten naar data die je kan 'store'n'
-$content.= "de inhoud uit de post";
-file_put_contents($file, $content);
-/*
-file_get_contents()
-    file_put_contents()
-json_encode() //to convert your array to a string to store.
-serialize()*/
-?>
 
+
+//array_unshift($array,$array_post);
+// make text from the data before putting it back on the json-file
+//$content=json_encode($array);
+//var_dump($content);  // om te testen
+
+?>
+<!-- VIEW -->
 <!doctype html>
 <html lang="en">
 <head>
@@ -104,7 +51,13 @@ serialize()*/
         .error{
             color:red;
         }
-
+        #post{
+            border: solid 1px gray;
+            margin: 10px;
+            padding:10px;
+            width: 100%;
+            background-color: gainsboro;
+        }
         footer {
             text-align: center;
         }
@@ -120,28 +73,41 @@ serialize()*/
     <em></em>
 <form method="post" action=" <?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?> " >
     <section class="form-row">
+
         <section class="form-group col-md-3">
             <label for="titel">Titel</label>
 
             <input type="text" id="title" name="title" class="form-control" placeholder="Here the titel" />
            <!-- <span class="error"> <?php //echo $titelErr; ?></span><br /> -->
-
-            <label for="message">Your message:</label>
+            <input type="hidden" id="date" name="date" value="<?php echo $date = date('d-m-Y - H:i:s'); ?>">
+            <label for="message" class="mt-3">Your message:</label>
             <textarea name="message" rows="10" cols="30" id="message" name="message" class="form-control" >
 Type here your message
 </textarea>
            <!-- value="<?php //echo $email; ?> -->
           <!--  <span class="error"> <?php //echo $messageErr; ?></span>-->
 
-            <label for="name">Your name</label>
+            <label for="name" class="mt-3">Your name</label>
             <input type="text" id="name" name="name" class="form-control" placeholder="Here your name" />
          <!--   <span class="error"> <?php //echo $nameErr; ?></span> -->
+            <button type="submit" class="btn btn-primary mt-3">Post comment</button>
         </section>
-    </section>
-<button type="submit" class="btn btn-primary">Post comment</button>
+
 </form>
+    </section>
+        <?php
+        // only show the latest 20 post - newest message on top
+        for ($i=0; $i<=20; $i++) {
+            echo "<section id='post' class='form-group col-md-6'>";
+            echo "<H4>".$post_values[$i]['titel']."</H4>";
+            echo "<em>".$post_values[$i]['date']."</em>";
+            echo "<p>".$post_values[$i]['comment']."</p>";
+            echo "<em>".$post_values[$i]['name']."</em>";
+            echo "</section>";
+        }
+        ?>
 </main>
-<footer>You have placed<?php //echo $_COOKIE['$cookie_email'] ?> your message.</footer>
+<footer><?php if($_POST){ echo "You have placed your message"; }else{ echo ""; } ?> </footer>
 </div>
 </body>
 </html>
