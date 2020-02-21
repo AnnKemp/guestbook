2d04afdec76bd010af15c7b9d063090a2330c59e
AnnKemp <ann.kemp@scarlet.be>
2020-02-21 16:59:49 +0100

    added seperate classes

---
+++ classes.php
@@ -0,0 +1,26 @@
+<?php
+// class post
+class post {
+
+    // initializing properties
+    private $author;
+    private $message_content;
+    private $message_title;
+    private $date;
+
+    public $postValues=[];
+
+    // this is the setter
+    public function __construct($title, $date, $message, $name) {
+        $this->message_title=$title;
+        $this->date= $date;
+        $this->message_content=$message;
+        $this->author = $name;
+        $this->postValues = array($this->message_title,$this->date,$this->message_content,$this->author);
+    }
+    // this is the getter
+    public function display() {
+        return $this->postValues;
+    }
+}
+
